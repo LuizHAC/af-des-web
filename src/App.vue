c7929391e754a7f1c4cfecc771de48c9f3c2d20e
LuizHAC <luizh5391@gmail.com>
2021-11-20 17:25:47 -0300

    Finished project

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <!-- App.vue -->
+
+  <v-app>
+    <v-system-bar app>
+      <!-- -->
+    </v-system-bar>
+
+    <v-app-bar app clipped>
+      <!-- -->
+    </v-app-bar>
+
+    <v-navigation-drawer app clipped>
+      <!-- -->
+    </v-navigation-drawer>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+
+    <v-bottom-navigation app>
+      <!-- -->
+    </v-bottom-navigation>
+  </v-app>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
